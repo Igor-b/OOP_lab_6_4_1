e5ac79d415e7eecb8df2c44a90dcd9b61ac572a4
Роман Данилевич <dwordchanel@gmail.com>
2020-05-31 14:36:32 +0300

    Add project files.

---
+++ OOP_lab_6_4_1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace OOP_lab_6_4_1
+{
+    class Program : IProgram
+    {
+        static void Main(string[] args)
+        {
+            new Program().Write();
+        }
+
+        public void Write()
+        {
+            Console.Write("Кiлькiсть записiв: ");
+
+            int n = int.Parse(Console.ReadLine());
+
+            Results[] work = new Results[n];
+
+            Console.WriteLine("{0, -10} {1, -30} {2, -50}", "Мiсяць", "План випуску продукцiї", "Фактичний випуск продукцiї");
+
+            string str;
+
+            for (int i = 0; i < n; ++i)
+            {
+                str = Console.ReadLine();
+
+                work[i] = new Results(str);
+            }
+
+            int difference = 0;
+
+            Console.WriteLine("\nНедовиконано:");
+
+            Console.WriteLine("{0, -10} {1, -30} {2, -50}", "Мiсяць", "План випуску продукцiї", "Фактичний випуск продукцiї");
+
+            for (int i = 0; i < n; ++i)
+            {
+                difference = Results.Compare(work[i].PlannedProductivity, work[i].Productivity);
+
+                if (difference > 0)
+                {
+                    Console.WriteLine("{0, -10} {1, -30} {2, -50}", work[i].Month, work[i].PlannedProductivity, work[i].Productivity);
+                }
+            }
+        }
+    }
+}
---
+++ OOP_lab_6_4_1/Results.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace OOP_lab_6_4_1
+{
+    class Results
+    {
+        private string _month;
+        private int _plannedProductivity;
+        private int _productivity;
+        
+        public string Month
+        {
+            get { return _month; }
+            set { _month = value; }
+        }
+        public int PlannedProductivity
+        {
+            get { return _plannedProductivity; }
+            set { _plannedProductivity = value; }
+        }
+        public int Productivity
+        {
+            get { return _productivity; }
+            set { _productivity = value; }
+        }
+
+        public Results()
+        {
+            Month = "Не вказано";
+            PlannedProductivity = 0;
+            Productivity = 0;
+        }
+
+        public Results(string str)
+        {
+            string[] elements = str.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            
+            Month = elements[0];
+            PlannedProductivity = int.Parse(elements[1]);
+            Productivity = int.Parse(elements[2]);
+        }
+
+        public static int Compare(int planproductivity, int realproductivity)
+        {
+            return planproductivity - realproductivity;
+        }
+    }
+}
